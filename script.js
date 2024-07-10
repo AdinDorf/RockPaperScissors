a6e193552a608ee50d4bf464ee1c72a12d4bb9c1
AdinDorf <adin@dorffamily.com>
2024-07-10 12:56:42 -0500

    Completed game loop

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
+let humanPoints = 0;
+let computerPoints = 0;
+const HU = "human";
+const CO = "computer";
+const T = "It's a tie!";
+
+playRound();
+////////////////////////////////////////////////
+//Function Declarations
+
 //Returns an integer between 0 and max
 function getRandomInt(max){
     return Math.floor(Math.random() * max)
 }
 
+//Returns a string containing the computer's move
 function getComputerMove()
 {
     let move = getRandomInt(3);
@@ -23,23 +34,71 @@ function getComputerMove()
      }
 }
 
+
 function getHumanMove()
 {
-    let input = window.prompt("Please enter your move (rock, paper, or scissors").trim().toLowerCase();
-    if (!isValidInput(str)){
+    let input = prompt("Please enter your move (rock, paper, or scissors)");
+    input = input.trim().toLowerCase();
+    if (!isValidInput(input)){
         console.log("Invalid input!");
-        getHumanMove();
+        return getHumanMove();
     }
     console.log("Recieved input: " + input);
+    return input;
 }
 
 function isValidInput(str)
 {
-    if (str === "rock" || str === "paper" || str == "scissors"){
+    if (str === "rock" || str === "paper" || str === "scissors"){
         return true
     }
-
     return false;
 }
 
+function determineWinner(computer, human)
+{
 
+    if (computer === human){
+        console.log("It's a tie!");
+        return T;
+    }
+
+    switch (computer){
+        case "rock":
+            if (human === "paper")
+            {
+                humanPoints++;
+                return HU;
+            }
+        case "paper":
+            if (human === "scissors")
+            {
+                humanPoints++;
+                return HU;
+            }
+        case "scissors":
+            if (human === "rock")
+            {
+                humanPoints++;
+                return HU;
+            }
+    }
+    
+    computerPoints++;
+    return CO;
+}
+
+function playRound()
+{
+    let computer = getComputerMove();
+    let human = getHumanMove();
+    let winner = determineWinner(computer, human);
+    console.log(`The human plays ${human} and the computer plays ${computer}`);
+    if (winner === T)
+    {
+        console.log(T);
+    }
+    else {
+        console.log(`The ${winner} wins!`);
+    }
+}
